f75a93ab17479c4776d1cc875623d95212cb678b
royghu201901 <royghu201901@hotmail.com>
2021-03-17 18:15:36 +0800

    完成头部banner和右侧个人信息模块

---
+++ blog/components/Banner/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './index.module.scss'
+import { Row, Col } from 'antd'
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <Row type="flex" justify="center">
+        <Col xs={0} sm={0} md={18} lg={18} xl={17} >
+          <img src="/static/homeBanner.png" alt="home banner" />
+          <div className={styles.wrapper}>
+            <p className={styles.title}>欢迎来到Roy的博客</p>
+            <p className={styles.description}>此博客使用React服务端框架Next.js渲染</p>
+            <a href="https://nextjs.org/" target="_blank">
+              <div className={styles.readMoreButton}>
+                <span>Read more</span>
+              </div>
+            </a>
+            <div className={styles.newArticalButton}>
+              <span>最近更新</span>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  )
+}
---
+++ blog/components/Info/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './index.module.scss'
+import {
+  GithubOutlined
+} from '@ant-design/icons'
+
+export default function Info() {
+  return (
+    <div className={styles.info}>
+      <img src="/static/rightColImg.png" alt="info img" />
+      <div className={styles.wrapper}>
+        <p className={styles.title}>Roy Gang Hu</p>
+        <p className={styles.description}>毕业于Teaxs A & M University</p>
+        <p className={styles.description}>现在杭州从事前端工作</p>
+        <a href="https://github.com/royghu201901">
+          <GithubOutlined style={{ fontSize: '2rem', color: '#fff' }} />
+        </a>
+      </div>
+    </div>
+  )
+}
