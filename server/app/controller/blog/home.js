b19b765655d04e36275eb892cf2565d1308d6559
royghu201901 <royghu201901@hotmail.com>
2021-03-31 15:27:36 +0800

    设置数据库数据，完成前后端数据传输

--- server/app/controller/blog/home.js
+++ server/app/controller/blog/home.js
@@ -7,6 +7,19 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = `Welcome to Roy's Blog`;
   }
+
+  async list() {
+    let sql = 'SELECT article.id as id ,' +
+      'article.title as title ,' +
+      'article.introduce as introduce ,' +
+      // "FROM_UNIXTIME(article.update_time, '%Y-%m-%d %H:%i:%s') as update_time ," +
+      'article.update_time as update_time ,' +
+      'article.view_count as view_count ,' +
+      'type.type_name as type_name ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.id'
+    const results = await this.app.mysql.query(sql)
+    this.ctx.body = {data: results}
+  }
 }
 
 module.exports = HomeController;
--- server/app/router/blog.js
+++ server/app/router/blog.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/blog/index', controller.blog.home.index);
+  router.get('/blog/list', controller.blog.home.list);
 }
 // 此处结构，router.js中就无需再结构，只要引入就可以了
